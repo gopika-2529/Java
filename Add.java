e70cc289d3a71b711acf3d2ac9b110b752f01ea6
gopika-2529 <gopikagopika51761@gmail.com>
2026-05-31 17:22:07 +0530

    Addition of two numbers in java

---
+++ Add.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class Add{
+	public static void main(String x [ ] ){
+	int a,b;
+	Scanner sc=new Scanner(System.in);
+	System.out.println("Enter Two Numbers:");
+	a=sc.nextInt();
+	b=sc.nextInt();
+	System.out.println("The Result Is:" +(a+b));
+	System.out.println("The Result Is:" +(a-b));
+	System.out.println("The Result Is:" +(a*b));
+	System.out.println("The Result Is:" +(a/b));
+	System.out.println("The Result Is:" +(a%b));
+		}
+}
